1d229d598614e00d707f3f309fe12a67cf39cab8
Yaron Reinharts <yaron.reinharts@gmail.com>
2022-02-26 09:51:28 +0200

    server from frontend

--- app.py
+++ app.py
@@ -1,26 +1,26 @@
 from flask import Flask, render_template, send_from_directory, request, jsonify, make_response
 from flask_cors import CORS, cross_origin
 
-app = Flask(__name__, static_folder='frontend/build', static_url_path='')
-cors = CORS(app)
+mishak_milim = Flask(__name__, static_folder='frontend/build', static_url_path='')
+cors = CORS(mishak_milim)
 
 
-@app.route('/api')
+@mishak_milim.route('/api')
 @cross_origin()
 def welcome():
     return "Welcome to the API!!!"
 
 
-@app.route('/api/generate/')
+@mishak_milim.route('/api/generate/')
 @cross_origin()
 def generate_word():
     return "Word"
 
 
-@app.route('/')
+@mishak_milim.route('/')
 def serve():
-    return send_from_directory(app.static_folder, 'index.html')
+    return send_from_directory(mishak_milim.static_folder, 'index.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    mishak_milim.run(host='0.0.0.0')
