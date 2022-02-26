07f5542e05adb42f64c3e6f1104ce30a05aaad09
Yaron Reinharts <yaron.reinharts@gmail.com>
2022-02-26 09:26:40 +0200

    Flask Empty

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, send_from_directory, request, jsonify, make_response
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__, static_folder='client/build', static_url_path='')
+cors = CORS(app)
+
+
+@app.route('/api')
+@cross_origin()
+def welcome():
+    return "Welcome to the API!!!"
+
+
+@app.route('/api/generate/')
+@cross_origin()
+def generate_word():
+    return "Word"
+
+
+@app.route('/')
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
