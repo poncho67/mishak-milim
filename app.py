ae8c4984b21fa79e1bdc5d13af01bc2bcee30cc1
Yaron Reinharts <yaron.reinharts@gmail.com>
2022-03-05 10:21:38 +0200

    Working with postgres

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
 import json
-
-from flask import Flask, render_template, send_from_directory, request, jsonify, make_response
-from flask_cors import CORS, cross_origin
 import random
 
+from flask import Flask, send_from_directory, request
+from flask_cors import CORS, cross_origin
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 mishak_milim = Flask(__name__, static_folder='frontend/build', static_url_path='')
+mishak_milim.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://yaron:postgres@localhost:5432/milim"
+mishak_milim.config['SQLALCHEMY_ENGINE_OPTIONS'] = False
+db = SQLAlchemy(mishak_milim)
+migrate = Migrate(mishak_milim, db)
 cors = CORS(mishak_milim)
+
+
+class WordsModel(db.Model):
+    __tablename__ = 'words'
+
+    id = db.Column(db.Integer, primary_key=True)
+    word = db.Column(db.String())
+
+    def __init__(self, word):
+        self.word = word
+
+    def __repr__(self):
+        return f"<Word {self.word}>"
+
 
 words = ["לילו", "שירו", "אוגר", "חתול", "כלב"]
 
@@ -25,6 +45,27 @@ def generate_word():
 @cross_origin()
 def welcome():
     return "Welcome to the API!!!"
+
+
+@mishak_milim.route('/word', methods=['POST', 'GET'])
+def handle_cars():
+    if request.method == 'POST':
+        if request.is_json:
+            data = request.get_json()
+            new_word = WordsModel(word=data['word'])
+            db.session.add(new_word)
+            db.session.commit()
+            return {"message": f"word {new_word.word} has been created successfully."}
+        else:
+            return {"error": "The request payload is not in JSON format"}
+
+    elif request.method == 'GET':
+        all_words = WordsModel.query.all()
+        n = random.randint(0, len(all_words) - 1)
+        response = {
+            'word': all_words[n].word
+        }
+        return json.dumps(response)
 
 
 @mishak_milim.route('/')
