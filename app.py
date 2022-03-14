10f2f2b1b8163f46d8857274e3bde77a362bc3dc
Yaron Reinharts <yaron.reinharts@gmail.com>
2022-03-15 00:21:51 +0200

    Using postgress - the old way

--- app.py
+++ app.py
@@ -1,32 +1,12 @@
 import json
 import random
 
+import psycopg2
 from flask import Flask, send_from_directory, request
 from flask_cors import CORS, cross_origin
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 
 mishak_milim = Flask(__name__, static_folder='frontend/build', static_url_path='')
-mishak_milim.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://yaron:postgres@localhost:5432/milim"
-mishak_milim.config['SQLALCHEMY_ENGINE_OPTIONS'] = False
-db = SQLAlchemy(mishak_milim)
-migrate = Migrate(mishak_milim, db)
 cors = CORS(mishak_milim)
-
-
-
-class WordsModel(db.Model):
-    __tablename__ = 'words'
-
-    id = db.Column(db.Integer, primary_key=True)
-    word = db.Column(db.String())
-
-    def __init__(self, word):
-        self.word = word
-
-    def __repr__(self):
-        return f"<Word {self.word}>"
-
 
 words = ["לילו", "שירו", "אוגר", "חתול", "כלב"]
 
@@ -34,7 +14,6 @@ words = ["לילו", "שירו", "אוגר", "חתול", "כלב"]
 @mishak_milim.route('/api/generate/')
 @cross_origin()
 def generate_word():
-
     n = random.randint(0, len(words) - 1)
     response = {
         'word': words[n]
@@ -48,25 +27,40 @@ def welcome():
     return "Welcome to the API!!!"
 
 
-@mishak_milim.route('/word', methods=['POST', 'GET'])
-def handle_cars():
-    if request.method == 'POST':
-        if request.is_json:
-            data = request.get_json()
-            new_word = WordsModel(word=data['word'])
-            db.session.add(new_word)
-            db.session.commit()
-            return {"message": f"word {new_word.word} has been created successfully."}
-        else:
-            return {"error": "The request payload is not in JSON format"}
+def handle_post(post_request):
+    if post_request.is_json:
+        conn = psycopg2.connect("postgresql://yaron:postgres@localhost:5432/milim")
+        cur = conn.cursor()
+        data = post_request.get_json()
+        cur.execute("insert into words (word) VALUES(%s)", (data['word'],))
+        conn.commit()
+        cur.close()
+        conn.close()
+        return {"message": f"word {data['word']} has been created successfully."}
+    else:
+        return {"error": "The request payload is not in JSON format"}
 
+
+def handle_get(get_request):
+    conn = psycopg2.connect("postgresql://yaron:postgres@localhost:5432/milim")
+    cur = conn.cursor()
+    sql = "SELECT word FROM words ORDER BY RANDOM() LIMIT 1"
+    cur.execute(sql)
+    row = cur.fetchall()
+    response = {
+        'word': row[0][0]
+    }
+    cur.close()
+    conn.close()
+    return json.dumps(response)
+
+
+@mishak_milim.route('/word', methods=['POST', 'GET'])
+def handle_words():
+    if request.method == 'POST':
+        return handle_post(request)
     elif request.method == 'GET':
-        all_words = WordsModel.query.all()
-        n = random.randint(0, len(all_words) - 1)
-        response = {
-            'word': all_words[n].word
-        }
-        return json.dumps(response)
+        return handle_get(request)
 
 
 @mishak_milim.route('/')
