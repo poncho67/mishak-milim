e65fdbd00a9e55502e31dd5611efe55454afa053
Yaron Reinharts <yaron.reinharts@gmail.com>
2022-03-18 22:11:13 +0200

    hebrew + right to left

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -51,30 +51,28 @@ class App extends Component {
            <div>
                 <Navbar bg="light" expand="lg">
                     <Container>
-                        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                        <Navbar.Brand href="#home">המשחקיה</Navbar.Brand>
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
                         <Navbar.Collapse id="basic-navbar-nav">
-                            <Nav className="me-auto">
-                                <Nav.Link href="#home">Home</Nav.Link>
-                                <Nav.Link href="#link">Link</Nav.Link>
-                                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                                      <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                      <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                      <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                      <NavDropdown.Divider />
-                                      <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            <Nav className="mr-auto">
+                                <Nav.Link href="#home">עמוד הבית</Nav.Link>
+                                <NavDropdown title="משחקים" id="basic-nav-dropdown">
+                                      <NavDropdown.Item href="#action/3.1">פנטומימה</NavDropdown.Item>
+                                      <NavDropdown.Item href="#action/3.2">קונטקט</NavDropdown.Item>
+                                      <NavDropdown.Item href="#action/3.3">כיד הדמיון</NavDropdown.Item>
                                 </NavDropdown>
+                                <Nav.Link href="#link">הוסף מילה</Nav.Link>
                             </Nav>
                         </Navbar.Collapse>
                     </Container>
                 </Navbar>
 
 
-               <button variant="primary" className="btn-primary" onClick={() => this.getWord()}> Get word </button>
+               <button variant="primary" className="btn-primary" onClick={() => this.getWord()}> חפש מילה </button>
                <div>{this.state.currentWord}</div>
-               <h2>Add new </h2>
+               <h2>הצע מילה חדשה </h2>
                <input onChange={this.onChangeHandler} type="text"/>
-               <button variant="secondary" onClick={this.onClickHandler}>Post word</button>
+               <button variant="secondary" onClick={this.onClickHandler}>שלח</button>
            </div>
        )
    }
