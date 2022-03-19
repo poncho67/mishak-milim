d6514477be1887e31ec5b22ce9eaea41be4b66df
Yaron Reinharts <yaron.reinharts@gmail.com>
2022-03-19 22:51:21 +0200

    Back to Navbar - as link to

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import Button from 'react-bootstrap/Button';
 import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
 import { LinkContainer } from 'react-router-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 
 
 class HomePage extends Component {
@@ -81,48 +81,31 @@ class AddWordPage extends Component {
 class NavigationBar extends Component {
     render() {
        return (
-            <Navbar bg="light" expand="lg">
-                <Container>
-                    <Navbar.Brand href="/Home">המשחקיה</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="mr-auto">
-                            <Nav.Link href="/Home">עמוד הבית</Nav.Link>
-                            <NavDropdown title="משחקים" id="basic-nav-dropdown">
-                                  <NavDropdown.Item href="/PantomimePage">פנטומימה</NavDropdown.Item>
-                                  <NavDropdown.Item href="#action/3.2">קונטקט</NavDropdown.Item>
-                                  <NavDropdown.Item href="#action/3.3">כיד הדמיון</NavDropdown.Item>
-                            </NavDropdown>
-                            <Nav.Link href="/AddWordPage">הוסף מילה</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
+            <div>
+                <Navbar bg="light" expand="lg">
+                    <Container>
+                        <Navbar.Brand as={Link} to={"/"}>המשחקיה</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="mr-auto">
+                                <Nav.Link as={Link} to={"pantomime"}>פנטומימה</Nav.Link>
+                                 <Nav.Link as={Link} to={"pantomime"}>קונטקט</Nav.Link>
+                                 <Nav.Link as={Link} to={"pantomime"}>כיד הדמיון</Nav.Link>
+                                 <Nav.Link as={Link} to={"add"}>הוסף מילה</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </div>
        );
     }
-}
-
-export const Home = () => {
-  return <div>You are in Home page</div>
-}
-export const About = () => {
-  return <div>This is the page where you put details about yourself</div>
-}
-export const NotFound = () => {
-  return <div>This is a 404 page</div>
 }
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <nav>
-                    <ul>
-                        <li> <Link to="/">המשחקיה</Link> </li>
-                        <li> <Link to="pantomime">פנטומימה</Link> </li>
-                        <li> <Link to="add">הוסף מילה</Link> </li>
-                    </ul>
-                </nav>
+                <NavigationBar />
                 <div className="main">
                     {/* Define all the routes */}
                     <Routes>
