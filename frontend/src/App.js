fbafcaddcbbc153ee7d663f5da338b8943cc3696
Yaron Reinharts <yaron.reinharts@gmail.com>
2022-03-19 22:19:06 +0200

    temporary remove Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 class HomePage extends Component {
     render() {
        return (
-        <h2>המשחקיה </h2>
+        <h2>המשחקיה</h2>
        )
     }
 }
@@ -29,7 +29,6 @@ class PantomimePage extends Component {
        var mydata = event.target.value;
        this.setState({postNewWord:mydata})
     }
-
 
     getWord() {
        fetch('./word', { method: 'GET' })
@@ -103,21 +102,40 @@ class NavigationBar extends Component {
     }
 }
 
-class App extends Component {
-   render() {
-       return (
-           <div>
-               <NavigationBar />
-               <Router>
-                   <Routes>
-                       <Route path="/Home" caseSensitive={false} element={<HomePage />} />
-                       <Route path="/PantomimePage" caseSensitive={false} element={<PantomimePage />} />
-                       <Route path="/AddWordPage" caseSensitive={false} element={<AddWordPage />} />
-                    </Routes>
-               </Router>
-           </div>
-       )
-   }
+export const Home = () => {
+  return <div>You are in Home page</div>
 }
+export const About = () => {
+  return <div>This is the page where you put details about yourself</div>
+}
+export const NotFound = () => {
+  return <div>This is a 404 page</div>
+}
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <nav>
+                    <ul>
+                        <li> <Link to="/">המשחקיה</Link> </li>
+                        <li> <Link to="pantomime">פנטומימה</Link> </li>
+                        <li> <Link to="add">הוסף מילה</Link> </li>
+                    </ul>
+                </nav>
+                <div className="main">
+                    {/* Define all the routes */}
+                    <Routes>
+                        <Route path="/" element={<HomePage />}></Route>
+                        <Route path="pantomime" element={<PantomimePage />}></Route>
+                        <Route path="add" element={<AddWordPage />}></Route>
+                        <Route path="*" element={<HomePage />}></Route>
+                    </Routes>
+                </div>
+            </div>
+        )
+    }
+}
+
 
 export default App;
