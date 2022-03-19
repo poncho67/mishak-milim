22c6f423e0f3a33747b05c107d062cd05a856b2b
Yaron Reinharts <yaron.reinharts@gmail.com>
2022-03-19 21:46:08 +0200

    Navbar with pages - not working locally

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import { useState } from 'react';
-import { MemoryRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
@@ -8,55 +8,26 @@ import { LinkContainer } from 'react-router-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 
-class NavigationBar extends Component {
+
+class HomePage extends Component {
     render() {
        return (
-            <Navbar bg="light" expand="lg">
-                <Container>
-                    <Navbar.Brand href="#home">המשחקיה</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="mr-auto">
-                            <Nav.Link href="#home">עמוד הבית</Nav.Link>
-                            <NavDropdown title="משחקים" id="basic-nav-dropdown">
-                                  <NavDropdown.Item href="#action/3.1">פנטומימה</NavDropdown.Item>
-                                  <NavDropdown.Item href="#action/3.2">קונטקט</NavDropdown.Item>
-                                  <NavDropdown.Item href="#action/3.3">כיד הדמיון</NavDropdown.Item>
-                            </NavDropdown>
-                            <Nav.Link href="#link">הוסף מילה</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-       );
+        <h2>המשחקיה </h2>
+       )
     }
 }
 
-class PantomimaPage extends Component {
+class PantomimePage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            currentWord: 'אוצר מילים',
-            postNewWord: "",
-            postResult: ""
+            currentWord: 'אוצר מילים'
         };
     }
 
     onChangeHandler=(event)=>{
        var mydata = event.target.value;
        this.setState({postNewWord:mydata})
-    }
-
-    onClickHandler=()=>{
-        const json_body = JSON.stringify({ word: this.state.postNewWord });
-        console.log('json_body: ' + json_body)
-        const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: json_body
-        };
-        fetch('./word', requestOptions)
-            .then(response => response.json())
     }
 
 
@@ -71,11 +42,64 @@ class PantomimaPage extends Component {
             <div>
                 <button variant="primary" className="btn-primary" onClick={() => this.getWord()}> חפש מילה </button>
                 <div>{this.state.currentWord}</div>
-                <h2>הצע מילה חדשה </h2>
-                <input onChange={this.onChangeHandler} type="text"/>
-                <button variant="secondary" onClick={this.onClickHandler}>שלח</button>
             </div>
         );
+    }
+}
+
+class AddWordPage extends Component {
+     constructor(props) {
+        super(props);
+        this.state = {
+            postNewWord: "",
+            postResult: ""
+        };
+    }
+
+     postNewWord=()=>{
+        const json_body = JSON.stringify({ word: this.state.postNewWord });
+        console.log('json_body: ' + json_body)
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: json_body
+        };
+        fetch('./word', requestOptions)
+            .then(response => response.json())
+    }
+
+     render() {
+        return (
+            <div>
+                <h2>הצע מילה חדשה </h2>
+                <input onChange={this.onChangeHandler} type="text"/>
+                <button variant="secondary" onClick={this.postNewWord}>שלח</button>
+            </div>
+        );
+    }
+}
+
+class NavigationBar extends Component {
+    render() {
+       return (
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand href="/Home">המשחקיה</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="mr-auto">
+                            <Nav.Link href="/Home">עמוד הבית</Nav.Link>
+                            <NavDropdown title="משחקים" id="basic-nav-dropdown">
+                                  <NavDropdown.Item href="/PantomimePage">פנטומימה</NavDropdown.Item>
+                                  <NavDropdown.Item href="#action/3.2">קונטקט</NavDropdown.Item>
+                                  <NavDropdown.Item href="#action/3.3">כיד הדמיון</NavDropdown.Item>
+                            </NavDropdown>
+                            <Nav.Link href="/AddWordPage">הוסף מילה</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+       );
     }
 }
 
@@ -84,7 +108,13 @@ class App extends Component {
        return (
            <div>
                <NavigationBar />
-               <PantomimaPage />
+               <Router>
+                   <Routes>
+                       <Route path="/Home" caseSensitive={false} element={<HomePage />} />
+                       <Route path="/PantomimePage" caseSensitive={false} element={<PantomimePage />} />
+                       <Route path="/AddWordPage" caseSensitive={false} element={<AddWordPage />} />
+                    </Routes>
+               </Router>
            </div>
        )
    }
